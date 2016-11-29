a7ceb4c7eaa21927698adb6a2875d5b321e08081
Lennart Jörelid <lj@jguru.se>
2016-11-29 23:37:21 +0100

    Fixed unit tests.

--- organisation/organisation-api/src/test/java/se/mithlond/services/organisation/api/parameters/ActivitySearchParametersTest.java
+++ organisation/organisation-api/src/test/java/se/mithlond/services/organisation/api/parameters/ActivitySearchParametersTest.java
@@ -32,49 +32,51 @@ import java.time.ZonedDateTime;
  */
 public class ActivitySearchParametersTest extends AbstractSearchParametersTest {
 
-	@Test
-	public void validateInitialState() {
+    @Test
+    public void validateInitialState() {
 
-		// Assemble
+        // Assemble
 
-		// Act
-		final ActivitySearchParameters unitUnderTest = ActivitySearchParameters.builder().build();
+        // Act
+        final ActivitySearchParameters unitUnderTest = ActivitySearchParameters.builder().build();
 
-		// Assert
-		validateEmpty(unitUnderTest.getOrganisationIDs());
-		validateEmpty(unitUnderTest.getActivityIDs());
-		validateEmpty(unitUnderTest.getMembershipIDs());
+        // Assert
+        validateEmpty(unitUnderTest.getOrganisationIDs());
+        validateEmpty(unitUnderTest.getActivityIDs());
+        validateEmpty(unitUnderTest.getMembershipIDs());
 
-		Assert.assertNotNull(unitUnderTest.getEndPeriod());
-		Assert.assertEquals(unitUnderTest.getStartPeriod(),
-				unitUnderTest.getEndPeriod().minus(ActivitySearchParameters.ActivitySearchParametersBuilder.DEFAULT_PERIOD_SIZE));
-		Assert.assertEquals("%", unitUnderTest.getFreeTextSearch());
-	}
+        Assert.assertNotNull(unitUnderTest.getEndPeriod());
+        Assert.assertEquals(unitUnderTest.getStartPeriod(),
+                unitUnderTest.getEndPeriod()
+                        .minus(ActivitySearchParameters.ActivitySearchParametersBuilder.DEFAULT_PERIOD_SIZE)
+                        .plusDays(1));
+        Assert.assertEquals("%", unitUnderTest.getFreeTextSearch());
+    }
 
-	@Test
-	public void validateParameterBuilding() {
+    @Test
+    public void validateParameterBuilding() {
 
-		// Assemble
-		final ZonedDateTime endPeriod = TimeFormat.YEAR_MONTH_DATE_HOURS_MINUTES.parse("2014-02-13 14:23");
-		final ActivitySearchParameters.ActivitySearchParametersBuilder builder =
-				ActivitySearchParameters.builder();
+        // Assemble
+        final ZonedDateTime endPeriod = TimeFormat.YEAR_MONTH_DATE_HOURS_MINUTES.parse("2014-02-13 14:23");
+        final ActivitySearchParameters.ActivitySearchParametersBuilder builder =
+                ActivitySearchParameters.builder();
 
-		// Act
-		final ActivitySearchParameters params = builder
-				.withOrganisationIDs(5L)
-				.withActivityIDs(25L, 65L)
-				.withFreeText("foobar")
-				.withEndPeriod(endPeriod.toLocalDateTime())
-				.build();
+        // Act
+        final ActivitySearchParameters params = builder
+                .withOrganisationIDs(5L)
+                .withActivityIDs(25L, 65L)
+                .withFreeText("foobar")
+                .withEndPeriod(endPeriod.toLocalDateTime())
+                .build();
 
-		// Assert
-		final String stringRepresentation = params.toString();
-		Assert.assertTrue(stringRepresentation.contains("[endPeriod       : 2014-02-13 14:23"));
-		Assert.assertTrue(stringRepresentation.contains("[startPeriod     : 2013-11-13 14:23"));
-		Assert.assertTrue(stringRepresentation.contains("[freeTextSearch  : foobar"));
+        // Assert
+        final String stringRepresentation = params.toString();
+        Assert.assertTrue(stringRepresentation.contains("[endPeriod       : 2014-02-13 14:23"));
+        Assert.assertTrue(stringRepresentation.contains("[startPeriod     : 2013-11-13 14:23"));
+        Assert.assertTrue(stringRepresentation.contains("[freeTextSearch  : foobar"));
 
-		Assert.assertEquals(0, params.getMembershipIDs().size());
-		validateContent(params.getOrganisationIDs(), 5);
-		validateContent(params.getActivityIDs(), 25, 65);
-	}
+        Assert.assertEquals(0, params.getMembershipIDs().size());
+        validateContent(params.getOrganisationIDs(), 5);
+        validateContent(params.getActivityIDs(), 25, 65);
+    }
 }
